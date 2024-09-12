747c90ff272dc634f6bc8220b18bbba3e65299d9
jae-2024 <dbswotkd2019@naver.com>
2024-09-13 02:51:03 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width = 500, height = 400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtle = []
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    #터틀이 출발선에서 보이게하려면 픽셀박스가 40,40 이므로 20을 빼줘야 보임
+    all_turtle.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! THE {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lose! THE {winning_color} turtle is the winner!")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
